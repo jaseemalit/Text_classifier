8fc61ad553c91bd86c262a96ec089e10fc012364
jaseemalit <132441260+jaseemalit@users.noreply.github.com>
2023-05-03 14:35:40 +0530

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -1,37 +1,40 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import streamlit as st
+import pickle
 
-bbc_text = pd.read_csv('bbc-text.txt')
-bbc_text=bbc_text.rename(columns = {'text': 'News_Headline'}, inplace = False)
-
+# Read the dataset
+bbc_text = pd.read_csv("bbc-text.txt")
+bbc_text = bbc_text.rename(columns={'text': 'News_Headline'}, inplace=False)
 bbc_text.category = bbc_text.category.map({'tech':0, 'business':1, 'sport':2, 'entertainment':3, 'politics':4})
 
-from sklearn.model_selection import train_test_split
+# Split the data
 X = bbc_text.News_Headline
 y = bbc_text.category
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.6, random_state=1)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.6, random_state = 1)
-
-from sklearn.feature_extraction.text import CountVectorizer
-
-vector = CountVectorizer(stop_words = 'english',lowercase=False)
-# fit the vectorizer on the training data
+# Vectorize the text
+vector = CountVectorizer(stop_words='english', lowercase=False)
 vector.fit(X_train)
-vector.vocabulary_
 X_transformed = vector.transform(X_train)
-X_transformed.toarray()
-# for test data
 X_test_transformed = vector.transform(X_test)
 
-from sklearn.naive_bayes import MultinomialNB
+# Train the model
 naivebayes = MultinomialNB()
 naivebayes.fit(X_transformed, y_train)
 
+# Save the model
+with open('model.pkl', 'wb') as f:
+    pickle.dump(naivebayes, f)
 
-import streamlit as st
-
-st.header('Text classifier')
-input = st.text_area("Please enter the text", value="")
-if st.button("Predict"):
-  vec = vector.transform(input).toarray()
-  print('Headline:', input)
-  print(str(list(naivebayes.predict(vec))[0]).replace('0', 'TECH').replace('1', 'BUSINESS').replace('2', 'SPORTS').replace('3','ENTERTAINMENT').replace('4','POLITICS'))
+# Define the Streamlit app
+st.header('Text Classification')
+input_text = st.text_area("Please enter the text", value="")
+if st.button("Check"):
+    input_text_transformed = vector.transform([input_text]).toarray()
+    prediction = naivebayes.predict(input_text_transformed)[0]
+    prediction_mapping = {0: 'TECH', 1: 'BUSINESS', 2: 'SPORTS', 3: 'ENTERTAINMENT', 4: 'POLITICS'}
+    result = prediction_mapping[prediction]
+    st.write(f"Predicted category: {result}")
